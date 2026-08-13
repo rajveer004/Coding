7e7636ba881f1823436d1fb33eb71a82c7be0074
rajveer004 <146945343+rajveer004@users.noreply.github.com>
2026-08-14 01:47:21 +0530

    Time: 4 ms (10.7%), Space: 13 MB (75.65%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -1,32 +1,28 @@
 class Solution {
-public:
-    int r;
-    int c;
-    vector<vector<int>> dp ;
-    int solve(int i,int j,vector<vector<int>> &dungeon){
-        if(i >= r || j >= c){
-            return 1e9;         // 200*200*1000 = 4*10 power 7 
-        }
-         if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(i == r-1 && j == c-1){
-            if(dungeon[i][j]<0){
-                return abs(dungeon[i][j])+1;
-            }else{
-                return 1;
+public://by tabulization
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int r =dungeon.size();
+        int c =dungeon[0].size();
+        vector<vector<int>> dp(r,vector<int>(c,-1));
+
+        for(int  i = r-1 ; i >= 0 ;i--){
+            for (int j = c-1 ;j >= 0;j--){
+                if(i == r-1 && j == c-1){
+                    dp[i][j] =(dungeon[i][j]>0)?1:abs(dungeon[i][j])+1;
+                }else if(i == r-1){
+                    int right = dp[i][j+1]-dungeon[i][j];
+                    dp[i][j] = (right >0)?right:1;
+                }else if(j == c-1){
+                    int down = dp[i+1][j]-dungeon[i][j];
+                    dp[i][j] =(down>0)?down:1;    
+                }else{
+                    int right = dp[i][j+1];
+                    int down = dp[i+1][j];
+                    int result =min(right,down) -dungeon[i][j];
+                    dp[i][j] = (result>0)?result:1;
+                }
             }
         }
-       
-        int right = solve(i,j+1,dungeon);
-        int down = solve(i+1,j,dungeon);
-        int result = min(right,down) - dungeon[i][j];
-        return dp[i][j] = (result>0)?result:1;
-    }
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-       r=dungeon.size(); 
-       c=dungeon[0].size();
-        dp = vector<vector<int>>(r,vector<int>(c,-1));
-        return solve(0,0,dungeon);
+        return dp[0][0];
     }
 };
