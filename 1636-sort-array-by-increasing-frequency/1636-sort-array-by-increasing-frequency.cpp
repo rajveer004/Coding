afdb7b86867edb6c7fb88eb5698fe6a2cb5aa562
rajveer004 <146945343+rajveer004@users.noreply.github.com>
2026-09-14 01:38:23 +0530

    Time: 0 ms (100%), Space: 15.5 MB (9.33%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int, int> freq;
-        for (int x : nums) freq[x]++;
 
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            if (freq[a] == freq[b])
-                return a > b;      // larger value first
-            return freq[a] < freq[b]; // smaller frequency first
-        });
+        // Count frequency
+        for (int x : nums)
+            freq[x]++;
 
-        return nums;
+        // frequency -> values
+        unordered_map<int, vector<int>> bucket;
+
+        for (auto &[num, f] : freq)
+            bucket[f].push_back(num);
+
+        vector<int> ans;
+
+        // Maximum frequency <= nums.size()
+        for (int f = 1; f <= nums.size(); f++) {
+            if (!bucket.count(f)) continue;
+
+            // Larger value first
+            sort(bucket[f].begin(), bucket[f].end(), greater<int>());
+
+            for (int num : bucket[f]) {
+                for (int i = 0; i < f; i++)
+                    ans.push_back(num);
+            }
+        }
+
+        return ans;
     }
 };
