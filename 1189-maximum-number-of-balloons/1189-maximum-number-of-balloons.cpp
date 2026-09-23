a030277fc8bc215f21dc05971a0a8d5807f0442e
rajveer004 <146945343+rajveer004@users.noreply.github.com>
2026-09-23 19:53:25 +0530

    Time: 0 ms (100%), Space: 9 MB (37.73%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int freq[26];
+        for(int i=0;i<text.length();i++){
+            freq[text[i]-'a']++;
+        }
+        return min({
+            freq['b'-'a'],
+            freq['a'-'a'],
+            freq['l'-'a']/2,
+            freq['o'-'a']/2,
+            freq['n'-'a'],
+        });
+    }
+};
